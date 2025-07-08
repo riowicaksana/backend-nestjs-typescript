ded10dae9badedd6c080304e61e423cef2a010cc
riowicaksana <riowicaksana34@gmail.com>
2025-07-09 06:38:53 +0700

    perubahan hostname4

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from "process"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
