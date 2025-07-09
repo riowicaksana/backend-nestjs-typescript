f6c9b1e94d55705bd62b4fd87c6f5f6298dd4620
RIO WICAKSANA <riowicaksana34@gmail.com>
2025-07-09 10:34:35 +0700

    disable globalPrefix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000,'0.0.0.0');
 }
 bootstrap();
